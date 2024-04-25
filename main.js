334bc30b1bbe998fcbea724879e2c49ce9ccd9a8
Fabio Santos <fsantos93x@gmail.com>
2024-04-25 00:40:24 -0300

    ok

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import { loadProducts } from "./src/loadProducts";
 import { filterProducts } from "./src/filter";
 import { getSearchProducts } from "./src/search";
 import { editAndSaveProduct } from "./src/editProd";
-import { exportData } from "./src/exports";
 
    
 loadProducts();
--- src/savings.js
+++ src/savings.js
@@ -20,3 +20,28 @@ export const removeLocalStorageProduct = (productName) => {
 
       localStorage.setItem("products", JSON.stringify(filteredProds))
 }
+
+
+// Exportar Dados
+const exportBtn = document.querySelector("#export");
+
+export function exportData() {
+      const notes = getProductsLocalStorage();
+
+      const csvString = [
+            ["Name", "Price", "Quantity", "Type"],
+            ...notes.map((prod) => [prod.nameInput, prod.priceInput, prod.quantityInput, prod.typePdtInput]),
+      ].map((e) => e.join(","))
+      .join("\n");
+      const element = document.createElement("a")
+      element.href = "data:text/csv;charset=utf-8," + encodeURI(csvString);
+
+      element.target = "blank";
+      element.download ="products.csv";
+
+      element.click();
+}
+
+exportBtn.addEventListener("click", () => {
+      exportData();
+})
