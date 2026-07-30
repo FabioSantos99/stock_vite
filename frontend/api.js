cd4b042f79c1f44ef9eafc9b71884ad1cb57c94f
FabioSantos99 <fsantos93x@gmail.com>
2026-07-30 20:43:35 -0300

    change links onrender

--- frontend/api.js
+++ frontend/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://stock-vite.onrender.com";
 
 // Envia o token JWT em toda requisição
 const authHeaders = () => ({
--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,5 @@
-const BASE_URL   = "http://localhost:3000";
+const BASE_URL = "https://stock-vite.onrender.com";
+
 const loginBtn   = document.querySelector("#login-btn");
 const loginError = document.querySelector("#login-error");
 
